d36cc693fd82a6e60eac167023fbaec211bd2ff9
marcoEDU <contact@marcobartsch.me>
2020-06-02 20:28:18 +0200

    added notion input support

--- downloadplus/downloader.py
+++ downloadplus/downloader.py
@@ -1,24 +1,29 @@
 import json
 import os
 
+from notion.client import NotionClient
+
 
 class Downloader():
     def __init__(self,
                  input_url=None,
                  input_json_path=None,
                  input_notion_url=None,
+                 notion_token=None,
                  target_main_directory=str(os.path.dirname(os.path.abspath(__file__)))+'/'):
 
         self.target_main_directory = target_main_directory
         self.input_url = input_url
         self.input_json_path = input_json_path
         self.input_notion_url = input_notion_url
+        self.notion_token = notion_token
         self.check_for_valid_input()
 
     def check_for_valid_input(self):
         if self.input_url:
             if type(self.input_url) != str:
                 raise TypeError
+
         elif self.input_json_path:
             with open(self.input_json_path) as json_file:
                 self.input_json_file = json.load(json_file)
@@ -27,12 +32,22 @@ class Downloader():
                     if 'url' not in entry or not entry['url']:
                         print('"url" key doesnt exist')
                         raise KeyError
+
         elif self.input_notion_url:
+            # check if token is given
+            if not self.notion_token:
+                print('notion_token is required to access Notion. Look in your cookies in your web browser for "token_v2" when visiting Notion - thats the notion_token')
+                raise PermissionError
+
             # check if a valid notion url
             if not self.input_notion_url.startswith('https://www.notion.so') and not self.input_notion_url.startswith('https://notion.so'):
                 print(
-                    'Notion URL isnt correctly formated. Make sure to start the URL with "https://www.notion.so"')
+                    'input_notion_url isnt correctly formated. Make sure to start the URL with "https://www.notion.so"')
                 raise SyntaxError
+
+            # get data and check if they are valid
+            self.notion_data = NotionClient(token_v2=self.notion_token).get_block(
+                self.input_notion_url).collection.get_rows()
 
     def download_file(self, url, name=None, target_subfolder=None):
         print('Start downloading file: {}'.format(url))
@@ -77,7 +92,7 @@ class Downloader():
             else:
                 self.download_file(self.input_url)
 
-        elif self.input_json_file:
+        elif self.input_json_path and self.input_json_file:
             # for every link from json: change status to "in progress", download file, change status to "downloaded" or "failed"
             for entry in self.input_json_file:
                 try:
@@ -106,8 +121,29 @@ class Downloader():
                     self.save_json()
 
         elif self.input_notion_url:
-            # TODO for every link from notion: change status to "in progress", download file, change status to "downloaded" or "failed"
-            pass
+            # for every link from notion: change status to "in progress", download file, change status to "downloaded" or "failed"
+            for entry in self.notion_data:
+                try:
+                    if entry.progress != 'in progress' and entry.progress != 'processed':
+                        entry.progress = 'in progress'
+
+                        if entry.url.startswith('magnet:'):
+                            self.download_torrent(
+                                magnet_link=entry.url,
+                                target_subfolder=entry.target_subfolder
+                            )
+                        else:
+                            self.download_file(
+                                url=entry.url,
+                                name=entry.name,
+                                target_subfolder=entry.target_subfolder
+                            )
+
+                        entry.progress = 'processed'
+                except:
+                    print(
+                        'Failed downloading file. Do you have the permission to download the file and is the url correct?')
+                    entry.progress = 'failed'
 
     def setup_torrent(self):
         # make sure npm is installed
